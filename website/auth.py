39aa6a13432f36ec7e1cf338e2ce98972ffe79b2
leulkashay <leulkashay2012@gmail.com>
2024-04-17 07:34:21 +0100

    first

---
+++ website/auth.py
@@ -0,0 +1,15 @@
+from flask import Blueprint
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+def login():
+    return '<p> login </p>'
+
+@auth.route('/logout')
+def login():
+    return '<p> logout </p>'
+
+@auth.route('/sign-up')
+def login():
+    return '<p> sign-up </p>'
